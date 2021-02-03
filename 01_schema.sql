079c0132336751d6a5da040f05b4c49edc219117
Bryce McLachlan <brycemclachlan@gmail.com>
2021-02-03 23:10:30 +0000

    Created schema for database

---
+++ 01_schema.sql
@@ -0,0 +1,39 @@
+CREATE TABLE users(
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(255),
+  email VARCHAR(255),
+  password VARCHAR(255)
+);
+CREATE TABLE properties(
+  id SERIAL PRIMARY KEY,
+  owner_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  title VARCHAR(255),
+  description VARCHAR(255),
+  thumbnail_photo_url VARCHAR(255),
+  cover_photo_url VARCHAR(255),
+  cost_per_night INT,
+  parking_spaces INT,
+  number_of_bathrooms INT,
+  number_of_bedrooms INT,
+  country VARCHAR(255),
+  street VARCHAR(255),
+  city VARCHAR(255),
+  province VARCHAR(255),
+  post_code VARCHAR(255),
+  active BOOLEAN
+);
+CREATE TABLE reservations(
+  id SERIAL PRIMARY KEY,
+  start_date DATE,
+  end_date DATE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE
+);
+CREATE TABLE property_reviews(
+  id SERIAL PRIMARY KEY,
+  guest_id INTEGER REFERENCES users(id) ON DELETE CASCADE,
+  property_id INTEGER REFERENCES properties(id) ON DELETE CASCADE,
+  reservation_id INTEGER REFERENCES reservations(id) ON DELETE CASCADE,
+  rating SMALLINT,
+  message TEXT
+);
